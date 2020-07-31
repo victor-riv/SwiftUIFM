355363d5a00dc93d3fe2fd892c5ee9f8f558bbd4
Victor Rivera <verm2405@gmail.com>
2020-07-31 12:52:34 -0400

    Init Project

--- SwiftUIFM/ContentView.swift
+++ SwiftUIFM/ContentView.swift
@@ -9,13 +9,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+
+  var actions: [Action] = [
+    Action(title: "Customer"),
+    Action(title: "Reader")
+  ]
+
+  var body: some View {
+    VStack {
+      ZStack {
+        VStack {
+          Text("Total")
+            .font(.custom("Lato-Regular", size: 20))
+            .foregroundColor(.white)
+          Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: 200)
+        .background(Color("fmOrange"))
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding(30)
+      }
+
+      VStack(spacing: 20) {
+        PaymentOptionCard(icon: "creditcard", title: "Pay")
+        PaymentOptionCard(icon: "creditcard", title: "Enter Payment Method")
+        PaymentOptionCard(icon: "creditcard", title: "Existing Payment Method")
+        PaymentOptionCard(icon: "creditcard", title: "Save As Invoice")
+        PaymentOptionCard(icon: "creditcard", title: "Cash")
+        PaymentOptionCard(icon: "creditcard", title: "Gift Card")
+      }
+      Spacer()
     }
+  }
+}
+
+
+struct Action: Identifiable {
+  var id = UUID()
+  var title: String
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct PaymentOptionCard: View {
+  var icon: String = ""
+  var title: String = ""
+
+  var body: some View {
+    HStack {
+      Image(systemName: icon)
+        .foregroundColor(.white)
+        .frame(width: 32, height: 32)
+
+      Text(title)
+        .foregroundColor(.white)
+      Spacer()
+      Image(systemName: "chevron.right")
+        .foregroundColor(.white)
+        .frame(width: 32, height: 32)
     }
+    .padding()
+    .frame(maxWidth: .infinity)
+    .background(Color("fmSkyBlue"))
+    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+    .padding(.horizontal, 30)
+  }
 }
