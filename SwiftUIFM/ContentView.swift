56af9383990e51b1f66fd01c9e75d6c749fd71bd
Victor Rivera <verm2405@gmail.com>
2020-07-31 11:07:43 -0400

    Initial Commit

---
+++ SwiftUIFM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFM
+//
+//  Created by Victor Rivera on 7/31/20.
+//  Copyright © 2020 Victor Rivera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
